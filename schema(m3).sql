a4a91bc190eac630e35e8f17b85faf4e9d07c4b0
Kh'ng Xuan Ming <khngxuanming@gmail.com>
2026-08-22 23:27:45 +0800

    Service and Counter

--- schema(m3).sql
+++ schema(m3).sql
@@ -23,14 +23,3 @@ CREATE TABLE Counter (
 );
 
 
--- =========================================
--- Queue history table
--- =========================================
-CREATE TABLE Queue_History (
-    queue_history_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    queue_id INTEGER NOT NULL,
-    status_changed_to TEXT NOT NULL,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (queue_id)
-    REFERENCES Queue(queue_id)
-);
