c3b5f30d572f5dce394c2c57512cb815c51201d7
EUGENE KWANG YU ZHE <egenekwang@gmail.com>
2026-08-22 18:26:23 +0800

    Eugene(Member 2): Add appointment, queue, and notification tables based on the agreed Excel schema

---
+++ schema.sql
@@ -0,0 +1,39 @@
+-- (Member 1 will write the CREATE TABLE user here...)
+-- (Member 1 will write the CREATE TABLE store here...)
+-- (Member 3 will write the CREATE TABLE service here...)
+-- (Member 3 will write the CREATE TABLE counter here...)
+
+-- Member 2(Eugene): appoinment table
+CREATE TABLE IF NOT EXISTS appointment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    service_id INTEGER NOT NULL,
+    appt_datetime DATETIME NOT NULL,
+    status TEXT DEFAULT 'BOOKED',
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (service_id) REFERENCES service(id)
+)
+
+-- Member 2(Eugene): queue table
+CREATE TABLE IF NOT EXISTS queue (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    service_id INTEGER NOT NULL,
+    counter_id INTEGER,
+    queue_number TEXT NOT NULL,
+    status TEXT DEFAULT 'WAITING',
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (service_id) REFERENCES service(id),
+    FOREIGN KEY (counter_id) REFERENCES counter(id)
+)
+
+-- (Member 3 will write the CREATE TABLE queue_history here...)
+
+-- Member 2(Eugene): notification table
+CREATE TABLE IF NOT EXISTS notification (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    message TEXT NOT NULL,
+    is_read BOOLEAN DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+)
