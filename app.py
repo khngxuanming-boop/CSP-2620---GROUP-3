3274cea54bd4ad034455f12f18e276f184ff52c0
normasyahmi <nsyahmi975@gmail.com>
2026-09-12 01:45:57 +0800

    another typo fix

--- app.py
+++ app.py
@@ -172,8 +172,7 @@ def store_details(store_id):
     store = conn.execute('SELECT * FROM store WHERE store_id =?', (store_id,)).fetchone()
 
     # Grab all active services linked to this store from Member 3's service table
-    services = conn.execute('SELECT * FROM service WHERE store_id = ?', (store_id)).fetchall()
-
+    services = conn.execute('SELECT * FROM service WHERE store_id = ?', (store_id,)).fetchall()
     conn.close()
 
     # Fall back error response if someone manually type a fake store ID in the URL
