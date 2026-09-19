c8ce20f973122ac1ce313c52d9dfb264b782d04a
EUGENE KWANG YU ZHE <egenekwang@gmail.com>
2026-09-19 19:24:09 +0800

    Eugene: add something for test

--- test_db.py
+++ test_db.py
@@ -1,5 +1,7 @@
 import sqlite3
 connection = sqlite3.connect('queue_system.db')
+connection.execute("PRAGMA foreign_keys = ON;")
+
 with open('schema.sql') as f:
     connection.executescript(f.read())
 cur = connection.cursor()
@@ -12,7 +14,10 @@ cur.execute("INSERT INTO user (username, password, role) VALUES ('testuser', 'pa
 
 # For Testing (Member 2)
 cur.execute("INSERT INTO service (store_id, service_name) VALUES (1, 'Card Authentication')")
-cur.execute("INSERT INTO counter (store_id, counter_name, counter_status) VALUES (1, 'Counter 1', 'Open')")
+cur.execute("INSERT INTO counter (store_id, counter_name, counter_status) VALUES (1, 'Counter 1', 'OPEN')")
+cur.execute("INSERT INTO appointment (user_id, service_id, appt_datetime, appt_status) VALUES (1, 1, '2026-09-20 14:00:00', 'BOOKED')")
+cur.execute("INSERT INTO queue (user_id, service_id, counter_id, queue_number, queue_status) VALUES (1, 1, NULL, 'A-001', 'WAITING')")
+cur.execute("INSERT INTO notification (user_id, message, is_read) VALUES (1, 'Your turn is approaching! There are 2 customers ahead.', 0)")
 
 connection.commit()
 connection.close()
